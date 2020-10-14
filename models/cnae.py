5f0b6a6d96e4b89af39048c6998b54ff061f9f66
Johann Neuhauser <johann@it-neuhauser.de>
2020-10-14 15:54:42 +0200

    CNAE: implement train_step and test_step and use ADModelEvaluator callback

--- models/cnae.py
+++ models/cnae.py
@@ -82,3 +82,28 @@ class CNAE(tf.keras.Model):
         x = self.net_enc(x, training=training)
         x = self.net_dec(x, training=training)
         return x
+
+    def train_step(self, data):
+        x, _, sample_weight = tf.keras.utils.unpack_x_y_sample_weight(data)
+        return super().train_step((x, x, sample_weight))
+
+    def test_step(self, data):
+        x, y, _ = tf.keras.utils.unpack_x_y_sample_weight(data)
+
+        x_pred = self(x, training=False)
+
+        #loss = self.compiled_loss(
+        #        x,
+        #        decoded_image,
+        #        sample_weight=sample_weight,
+        #        regularization_losses=self.losses,
+        #    )
+
+        # we need a loss value per image which isn't provided by compiled_loss
+        # assume we always have a shape of (batch_size, width, height, depth)
+        losses = tf.keras.backend.mean(tf.keras.backend.square(x - x_pred), axis=[1,2,3])
+
+        return {
+            "losses": tf.reshape(losses, (-1, 1)),
+            "labels": tf.reshape(y, (-1, 1))
+            }
